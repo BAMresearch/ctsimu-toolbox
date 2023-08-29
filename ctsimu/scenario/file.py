6e46ab203a1de901665dd15a28c792852436306d
David Plotzki <david@tastyorange.de>
2023-08-29 21:41:06 +0200

    scenario.file: auto-initialize with current date.

--- ctsimu/scenario/file.py
+++ ctsimu/scenario/file.py
@@ -2,6 +2,7 @@
 """
 Collection of parameters that describe the scenario file.
 """
+from datetime import datetime
 
 from ..helpers import *
 from ..geometry import *
@@ -13,13 +14,15 @@ class File(Group):
 	def __init__(self, _root=None):
 		Group.__init__(self, name="file", _root=_root)
 
+		now = datetime.now()
+
 		self.set(key="file_type", value="CTSimU Scenario", native_unit="string", simple=True)
 
 		self.set(key="name", value=None, native_unit="string", simple=True)
 		self.set(key="description", value=None, native_unit="string", simple=True)
 		self.set(key="contact", value=None, native_unit="string", simple=True)
-		self.set(key="date_created", value=None, native_unit="string", simple=True)
-		self.set(key="date_changed", value=None, native_unit="string", simple=True)
+		self.set(key="date_created", value=now.strftime("%Y-%m-%d"), native_unit="string", simple=True)
+		self.set(key="date_changed", value=now.strftime("%Y-%m-%d"), native_unit="string", simple=True)
 
 		# version
 		self.new_subgroup("version")
